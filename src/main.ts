c83e9b3bd517eacac8bec1318a13f247b3c8fbe3
Eren Küçük <erenkucuk2002@gmail.com>
2023-06-12 23:15:31 +0300

    Vuetify added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+
+loadFonts();
+
+createApp(App).use(vuetify).mount("#app");
